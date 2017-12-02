d57e54c578f796e6ed2148b41248d1f111cccbaa
Neo1ite <neo1515@gmail.com>
2017-12-02 13:32:31 +0300

    Some fixes for Win platform

--- _sha2.c
+++ _sha2.c
@@ -1,7 +1,7 @@
 /*
  * FILE:	sha2.h
  * AUTHOR:	Aaron D. Gifford <me@aarongifford.com>
- * 
+ *
  * Copyright (c) 2000-2001, Aaron D. Gifford
  * All rights reserved.
  *
@@ -16,7 +16,7 @@
  * 3. Neither the name of the copyright holder nor the names of contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTOR(S) ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -49,8 +49,18 @@ extern "C" {
 #include <string.h>	/* memcpy()/memset() or bcopy()/bzero() */
 /* #include <assert.h> */   /* assert() */
 #include <stdio.h>
-#include <sysexits.h>
-#include <sys/uio.h>
+#ifdef _WIN32
+/*
+ * Definitions for byte order, according to byte significance from low
+ * address to high.
+ */
+#define LITTLE_ENDIAN  1234    /* LSB first: i386, vax */
+#define BIG_ENDIAN     4321    /* MSB first: 68000, ibm, net */
+#define PDP_ENDIAN     3412    /* LSB first in word, MSW first in long */
+
+#define BYTE_ORDER     LITTLE_ENDIAN
+#endif
+//#include <sys/uio.h>
 #include <unistd.h>
 
 #ifdef SHA2_USE_INTTYPES_H
@@ -78,8 +88,8 @@ extern "C" {
  * for your system:
  */
 #if 0
-typedef unsigned char u_int8_t;		/* 1-byte  (8-bits)  */
-typedef unsigned int u_int32_t;		/* 4-bytes (32-bits) */
+typedef unsigned char u_int8_t;		    /* 1-byte  (8-bits)  */
+typedef unsigned int u_int32_t;		    /* 4-bytes (32-bits) */
 typedef unsigned long long u_int64_t;	/* 8-bytes (64-bits) */
 #endif
 /*
@@ -90,7 +100,7 @@ typedef unsigned long long u_int64_t;	/* 8-bytes (64-bits) */
  *
  *   #include <inttypes.h>
  *
- * If you choose to use <inttypes.h> then please define: 
+ * If you choose to use <inttypes.h> then please define:
  *
  *   #define SHA2_USE_INTTYPES_H
  *
@@ -204,7 +214,7 @@ char* SHA512_Data();
 /*
  * FILE:	sha2.c
  * AUTHOR:	Aaron D. Gifford <me@aarongifford.com>
- * 
+ *
  * Copyright (c) 2000-2001, Aaron D. Gifford
  * All rights reserved.
  *
@@ -219,7 +229,7 @@ char* SHA512_Data();
  * 3. Neither the name of the copyright holder nor the names of contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTOR(S) ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -275,7 +285,7 @@ char* SHA512_Data();
  *
  * And for little-endian machines, add:
  *
- *   #define BYTE_ORDER LITTLE_ENDIAN 
+ *   #define BYTE_ORDER LITTLE_ENDIAN
  *
  * Or for big-endian machines:
  *
@@ -306,13 +316,13 @@ char* SHA512_Data();
  */
 #ifdef SHA2_USE_INTTYPES_H
 
-typedef uint8_t  sha2_byte;	/* Exactly 1 byte */
+typedef uint8_t  sha2_byte;		/* Exactly 1 byte */
 typedef uint32_t sha2_word32;	/* Exactly 4 bytes */
 typedef uint64_t sha2_word64;	/* Exactly 8 bytes */
 
 #else /* SHA2_USE_INTTYPES_H */
 
-typedef u_int8_t  sha2_byte;	/* Exactly 1 byte */
+typedef u_int8_t  sha2_byte;		/* Exactly 1 byte */
 typedef u_int32_t sha2_word32;	/* Exactly 4 bytes */
 typedef u_int64_t sha2_word64;	/* Exactly 8 bytes */
 
@@ -683,11 +693,11 @@ void SHA256_Transform(SHA256_CTX* context, const sha2_word32* data) {
 		/* Part of the message block expansion: */
 		s0 = W256[(j+1)&0x0f];
 		s0 = sigma0_256(s0);
-		s1 = W256[(j+14)&0x0f];	
+		s1 = W256[(j+14)&0x0f];
 		s1 = sigma1_256(s1);
 
 		/* Apply the SHA-256 compression function to update a..h */
-		T1 = h + Sigma1_256(e) + Ch(e, f, g) + K256[j] + 
+		T1 = h + Sigma1_256(e) + Ch(e, f, g) + K256[j] +
 		     (W256[j&0x0f] += s1 + W256[(j+9)&0x0f] + s0);
 		T2 = Sigma0_256(a) + Maj(a, b, c);
 		h = g;
@@ -1263,128 +1273,3 @@ char* SHA384_Data(const sha2_byte* data, size_t len, char digest[SHA384_DIGEST_S
 	SHA384_Update(&context, data, len);
 	return SHA384_End(&context, digest);
 }
-
-/*
- * FILE:	sha2prog.c
- * AUTHOR:	Aaron D. Gifford <me@aarongifford.com>
- * 
- * Copyright (c) 2000-2001, Aaron D. Gifford
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTOR(S) ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTOR(S) BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * $Id: sha2prog.c,v 1.1 2001/11/08 00:02:11 adg Exp adg $
- */
-
-void usage(char *prog, char *msg) {
-	fprintf(stderr, "%s\nUsage:\t%s [options] [<file>]\nOptions:\n\t-256\tGenerate SHA-256 hash\n\t-384\tGenerate SHA-284 hash\n\t-512\tGenerate SHA-512 hash\n\t-ALL\tGenerate all three hashes\n\t-q\tQuiet mode - only output hexadecimal hashes, one per line\n\n", msg, prog);
-	exit(-1);
-}
-
-#define BUFLEN 16384
-
-int main(int argc, char **argv) {
-	int		kl, l, fd, ac;
-	int		quiet = 0, hash = 0;
-	char		*av, *file = (char*)0;
-	FILE		*IN = (FILE*)0;
-	SHA256_CTX	ctx256;
-	SHA384_CTX	ctx384;
-	SHA512_CTX	ctx512;
-	unsigned char	buf[BUFLEN];
-
-	SHA256_Init(&ctx256);
-	SHA384_Init(&ctx384);
-	SHA512_Init(&ctx512);
-
-	/* Read data from STDIN by default */
-	fd = fileno(stdin);
-
-	ac = 1;
-	while (ac < argc) {
-		if (*argv[ac] == '-') {
-			av = argv[ac] + 1;
-			if (!strcmp(av, "q")) {
-				quiet = 1;
-			} else if (!strcmp(av, "256")) {
-				hash |= 1;
-			} else if (!strcmp(av, "384")) {
-				hash |= 2;
-			} else if (!strcmp(av, "512")) {
-				hash |= 4;
-			} else if (!strcmp(av, "ALL")) {
-				hash = 7;
-			} else {
-				usage(argv[0], "Invalid option.");
-			}
-			ac++;
-		} else {
-			file = argv[ac++];
-			if (ac != argc) {
-				usage(argv[0], "Too many arguments.");
-			}
-			if ((IN = fopen(file, "r")) == NULL) {
-				perror(argv[0]);
-				exit(-1);
-			}
-			fd = fileno(IN);
-		}
-	}
-	if (hash == 0)
-		hash = 7;	/* Default to ALL */
-
-	kl = 0;
-	while ((l = read(fd,buf,BUFLEN)) > 0) {
-		kl += l;
-		SHA256_Update(&ctx256, (unsigned char*)buf, l);
-		SHA384_Update(&ctx384, (unsigned char*)buf, l);
-		SHA512_Update(&ctx512, (unsigned char*)buf, l);
-	}
-	if (file) {
-		fclose(IN);
-	}
-
-	if (hash & 1) {
-		SHA256_End(&ctx256, buf);
-		if (!quiet)
-			printf("SHA-256 (%s) = ", file);
-		printf("%s\n", buf);
-	}
-	if (hash & 2) {
-		SHA384_End(&ctx384, buf);
-		if (!quiet)
-			printf("SHA-384 (%s) = ", file);
-		printf("%s\n", buf);
-	}
-	if (hash & 4) {
-		SHA512_End(&ctx512, buf);
-		if (!quiet)
-			printf("SHA-512 (%s) = ", file);
-		printf("%s\n", buf);
-	}
-
-	return 0;
-}
-
